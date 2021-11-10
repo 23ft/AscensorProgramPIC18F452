0841eaf3d6678e540091c43d344bb696a80c1284
23ft <23ft.deaza@gmail.com>
2021-11-10 09:16:39 -0500

    Upgrade 10.1416

--- AscensorProgramGit.X/main.c
+++ AscensorProgramGit.X/main.c
@@ -12,6 +12,15 @@ Cambios pendientes [8 Nov]
  
 */
 
+/*
+ 
+ [Pruebas pendietes]
+ * Hardware sistema de puertas y prueba de software.
+ * Prueba de inicializacion ascensor para determinar pines UP_ASC y DOWN_ASC
+ * Prueba interrupciones en hardware con todos los botones de piso.
+ 
+ 
+ */
 
 
 
@@ -68,12 +77,12 @@ Cambios pendientes [8 Nov]
 #define _XTAL_FREQ 20000000
 
 /* Switchs for floor */
-#define btnUpFL1 PORTBbits.RB6
+#define btnUpFL1 PORTBbits.RB0
 #define btnUpFL2 PORTBbits.RB1
 #define btnDownFL2 PORTBbits.RB2
-#define btnUpFL3 PORTBbits.RB3
-#define btnDownFL3 PORTBbits.RB4
-#define btnDownFL4 PORTBbits.RB5
+#define btnUpFL3 PORTBbits.RB4
+#define btnDownFL3 PORTBbits.RB5
+#define btnDownFL4 PORTBbits.RB6
 
 /* Switchs User panel  */
 #define btnPn1 PORTAbits.RA0
@@ -82,7 +91,7 @@ Cambios pendientes [8 Nov]
 #define btnPn4 PORTAbits.RA3
 #define btnPnCD PORTAbits.RA4
 #define btnPnOD PORTAbits.RA5
-#define btnPnSTP PORTBbits.RB0
+#define btnPnSTP PORTBbits.RB7
 
 /* Sensors hall */
 #define SenFL1 PORTDbits.RD4
@@ -106,9 +115,12 @@ Cambios pendientes [8 Nov]
 #define FLOOR3 3
 #define FLOOR4 4
 
-int calls, callsD, callFL, nowFL, queueUp[100], queueDown[100], callsInUp[100], numUps;
+int tasksDown, tasksUp, calls, callsD, callFL, nowFL, queueUp[100], queueDown[100], callsInUp[100];
 
-unsigned int upF = 0, downF = 0;    // flags.
+unsigned int *UpTasks = %queueUp[0];        
+unsigned int modeUp_F, modeDown_F, btn1_f, btn2_f, btn3_f, btn4_f, btnCD_f, btnOD_f;    
+unsigned int up1_f, up2_f, down2_f, up3_f, down3_f, down4_f;
+     
 
 /* Prototypes functions */
 #define size(x, type) (sizeof(x) / sizeof(type))
@@ -131,6 +143,34 @@ void boot() {
     TRISD = 0xF0;           // RD0-RD3 (Outputs), RD4-RD7 (Inputs).   
     TRISA = 0xFF;           // Registro TRISA definimos todas como salidas.
     TRISC = 0xFC;           // Registro TRISC definimos RC0-RC1 salidas, RC2-RC7 entradas.
+
+    /* Reference of variables */
+    tasksDown = 0;       // Number tasks for mode down.
+    tasksUp = 0;         // Number tasks for mode up     
+    calls = 0;           // Calls floors in mode up.
+    callsD = 0;          // Calls floors in mode Down.
+    callFL = 0;          // Piso de la ultima llamada.
+    nowFL = 0;           // Last floor stoped.
+    
+    /* Flags */
+    modeUp_F = 0;       // Mode up flag
+    modeDown_F = 0;     // Mode down flag
+    
+    btn1_f = 0;         // btn1 panel flag
+    btn2_f = 0;         // btn2 panel flag
+    btn3_f = 0;         // btn3 panel flag
+    btn4_f = 0;         // btn4 panel flag
+    btnCD_f = 0;        // btn CloseDoor panel flag
+    btnOD_f = 0;        // btn OpenDoor panel flag
+        
+    up1_f = 0;          // btn floor 1 up.
+    up2_f = 0;          // btn floor 2 up.    
+    down2_f = 0;        // btn floor 2 down.
+    up3_f = 0;          // btn floor 3 up.
+    down3_f = 0;        // btn floor 3 down.
+    down4_f  = 0;       // btn floor 4 down.
+
+    UpTasks;            // Pointer to array of task in mode up.
 }
 
 void interruptsInit(void) {
@@ -202,52 +242,77 @@ void sort(int *p, int sizes) {
 void dataPanelUp() {
     do {
         if (btnPn1) {
-            if (nowFL < FLOOR1) {
-                queueUp[calls] = FLOOR1;
-                calls++;
-            } else {
-                queueDown[callsD] = FLOOR1;
-                callsD++;
+            
+            if (!btn1_f){
+                if (nowFL < FLOOR1) {
+                    queueUp[tasksUp] = FLOOR1;
+                    tasksUp++;
+                    btn1_f = 1;            // set flag to btn1 in panel.
+                } else {
+                    queueDown[tasksDown] = FLOOR1;
+                    callsD++;
+                    btn1_f = 1;
+                }
             }
-
+            
         }
         if (btnPn2) {
-            if (nowFL < FLOOR2) {
-                queueUp[calls] = FLOOR2;
-                calls++;
-            } else {
-                queueDown[callsD] = FLOOR2;
-                callsD++;
-            }
+            if (!btn2_f){
 
+                if (nowFL < FLOOR2) {
+                    queueUp[tasksUp] = FLOOR2;
+                    tasksUp++;
+                    btn2_f = 1;             // set flag to btn2 in panel.
+                } else {
+                    queueDown[tasksDown] = FLOOR2;
+                    callsD++;
+                    btn2_f = 1;
+                }
+
+            }
         }
         if (btnPn3) {
-            if (nowFL < FLOOR3) {
-                queueUp[calls] = FLOOR3;
-                calls++;
-            } else {
-                queueDown[callsD] = FLOOR3;
-                callsD++;
+            if (!btn3_f){
+
+                if (nowFL < FLOOR3) {
+                    queueUp[tasksUp] = FLOOR3;
+                    tasksUp++;
+                    btn3_f = 1;
+                } else {
+                    queueDown[tasksDown] = FLOOR3;
+                    callsD++;
+                    btn3_f = 1;
+
+                }
             }
 
         }
         if (btnPn4) {
-            if (nowFL < FLOOR4) {
-                queueUp[calls] = FLOOR4;
-                calls++;
-            } else {
-                queueDown[callsD] = FLOOR4;
-                callsD++;
-            }
+            if (!btn4_f){
 
+                if (nowFL < FLOOR4) {
+                    queueUp[tasksUp] = FLOOR4;
+                    tasksUp++;
+                    btn4_f = 1;
+                } else {
+                    queueDown[tasksDown] = FLOOR4;
+                    callsD++;
+                    btn4_f = 1;
+
+                }
+            }
         }
     } while (!btnPnCD);
 
-    int *UpCalls = &queueUp[0];
-    int tamano = calls;
+    
+  
 
-    sort(UpCalls, tamano);
+    sort(UpCalls, tasksUp);
 
+}
+
+void modeDownControl(){
+    
 }
 
 void modeUpControl() {
@@ -257,12 +322,17 @@ void modeUpControl() {
     switch (queueUp[cont]) {
         case FLOOR1:
             if (SenFL1) {
+                
+                btn1_f = 0;     // clear flag button in panel.
+                
                 // STOP in floor1
                 DOWN_ASC = 1;
-                for (int i = 0; i < calls; i++) {
+                
+                
+                for (int i = 0; i < calls; i++) {       // clear call 0 in array of calls.
                     queueUp[i] = queueUp[i + 1];
                 }
-                calls--;
+                tasksUp--;
                 DOOR1 = 1;
                 nowFL = FLOOR1;
                 
@@ -277,6 +347,9 @@ void modeUpControl() {
 
         case FLOOR2:
             if (SenFL2) {
+                
+                btn2_f = 0;     // clear flag button in panel.
+
                 // STOP in floor2
                 DOWN_ASC = 1;
                 DOOR2 = 1;
@@ -285,7 +358,7 @@ void modeUpControl() {
                 for (int i = 0; i < calls; i++) {
                     queueUp[i] = queueUp[i + 1];
                 }
-                calls--;
+                tasksUp--;
                 dataPanelUp();
                 DOOR2 = 0;
                 __delay_ms(300);
@@ -296,6 +369,9 @@ void modeUpControl() {
 
         case FLOOR3:
             if (SenFL3) {
+                
+                btn3_f = 0;     // clear flag button in panel.
+
                 // STOP in floor3
                 DOWN_ASC = 1;
                 DOOR3 = 1;
@@ -304,7 +380,7 @@ void modeUpControl() {
                 for (int i = 0; i < calls; i++) {
                     queueUp[i] = queueUp[i + 1];
                 }
-                calls--;
+                tasksUp--;
                 dataPanelUp();
                 DOOR3 = 0;
                 __delay_ms(300);
@@ -315,6 +391,10 @@ void modeUpControl() {
 
         case FLOOR4:
             if (SenFL4) {
+                
+                btn4_f = 0;     // clear flag button in panel.
+
+                
                 // STOP in floor4
                 DOWN_ASC = 1;
                 DOOR4 = 1;
@@ -323,7 +403,7 @@ void modeUpControl() {
                 for (int i = 0; i < calls; i++) {
                     queueUp[i] = queueUp[i + 1];
                 }
-                calls--;
+                tasksUp--;
                 dataPanelUp();
                 DOOR4 = 0;
                 __delay_ms(300);
@@ -335,14 +415,25 @@ void modeUpControl() {
 
 }
 
+void modeDown() {
+    modeDown_F = 1;
+    callsD = 0;
+    
+}
+
 void modeUp() {
-    upF = 1;        // Set flag mode up.
+    modeUp_F = 1;        // Set flag mode up.
+    
+    /*Contadores llamadas a piso */
     callsD = 0;
     calls = 0;
-    numUps = 0;
+    
+    /*Numero de tareas en cola */
+    tasksUp = 0;
+    tasksDown = 0;
 
     switch (callFL) {
-        case 1:
+        case FLOOR1:
 
             if (nowFL == FLOOR1) {
                 DOOR1 = 1;
@@ -357,8 +448,19 @@ void modeUp() {
 
                     modeUpControl();
                     
+                    if (tasksUp == 0){        // when the calls is complete...
+                        if(callsD > 0){
+                            
+                        }else{
+                            /* No calls en down mode and up mode, init waiting mode */
+                            callFL = 0;
+                            modeUp_F = 0;
+                            break;
+                        }
+                    }
                 }
-
+            
+                /* CORRECCION A HACER: definir en que piso voy y determinar si subo o bajo. */
             } else {
                 do {
                     UP_ASC = 0;
@@ -390,6 +492,11 @@ void main(void) {
             callFL = FLOOR3;
             modeUp();
         }
+        
+        if(btnDownFL2){
+            
+        }
+        
     }
 
     return;
@@ -398,35 +505,77 @@ void main(void) {
 
 //I use a intrrupts in INT0-INT1-INT2 and changes in portb with RB port change interrupt.
 void __interrupt() ISR() {
+    
     switch (PORTB) {
-        case 0x00: // (RB0) Button up 1 is press.
-            LATD = 0x00;
+        case 0x01: // (RB0) Button up 1 is press.
+            if (modeUp_F){             
+                if(!btn1_f){
+                    if (nowFL < FLOOR1){
+                        queueUp[tasksUp] = FLOOR1;
+                        tasksUp++;
+                        sort(UpCalls, tasksUp);
+
+                    }
+                }
+            }
             break;
 
         case 0x02: // (RB1) button Up2 press 0x02 is 2 in decimal. 
-            callsInUp[numUps] = FLOOR2;
-            numUps++;
+            if (modeUp_F){
+                if(!btn2_f){
+                    if (nowFL < FLOOR2){
+                        queueUp[tasksUp] = FLOOR2;
+                        tasksUp++;
+                        sort(UpCalls, tasksUp);
+                    }
+                }
+            }
             break;
 
         case 0x04: // (RB2) button Down2 press 0x04 is 4 in decimal.
-            
+            if (modeUp_F){
+                if(!down2_f){
+                    callsInUp[callsD] = FLOOR2;
+                    callsD++;
+                    down2_f = 1;    // set flag button dwon floor 2.
+                }
+            }
             break;
 
         case 0x08: // (RB3) not use.
-            callsInUp[numUps] = FLOOR3;
-            numUps++;
+            
             break;
-
+            
         case 0x10: // (RB4) button Up3 press 0x08 is 8 in decimal.
-            LATD = 0x10;
+            if (modeUp_F){
+                if(!btn3_f){
+                    if (nowFL < FLOOR3){
+                        queueUp[tasksUp] = FLOOR3;
+                        tasksUp++;
+                        sort(UpCalls, calls);
+                    }
+                }
+            }
             break;
 
         case 0x20: // (RB5) button Down3
-            LATD = 0x20;
+            if (modeUp_F){
+                if(!down3_f){
+                    callsInUp[callsD] = FLOOR3;
+                    callsD++;
+                    down3_f = 1;    // set flag button dwon floor 3.
+                }
+            }
             break;
 
-        case 0x40: // (RB6)button down4
-            LATD = 0x40;
+        case 0x40: // (RB6)button down4 
+            if (modeUp_F){
+                if(!down4_f){
+                    callsInUp[callsD] = FLOOR4;
+                    callsD++;
+                    down4_f = 1;    // set flag button dwon floor 4.
+                }
+            }
             break;
             
         case 0x80: // (RB7) PNL_BTN_STP
